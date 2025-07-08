3e0a83df3dd280cdee35a24d609e2226db58fa80
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2025-07-08 13:56:32 -0400

    update mock mosques location to make more accurate

--- app/mosque/map.tsx
+++ app/mosque/map.tsx
@@ -11,22 +11,22 @@ const MOCK_MOSQUES = [
     id: "1",
     name: "Ar-Rahman",
     address: "333 86th St, Brooklyn, NY 11209",
-    lat: 40.6234,
-    lng: -74.0306,
+    lat: 40.623566,
+    lng: -74.029900,
   },
   {
     id: "2",
     name: "Islamic Society of Bay Ridge",
     address: "6807 5th Ave, Brooklyn, NY 11220",
-    lat: 40.6358,
-    lng: -74.0243,
+    lat: 40.633808,
+    lng: -74.0206412,
   },
   {
     id: "3",
     name: "Maryam Mosque",
     address: "7307 5th Ave, Brooklyn, NY 11209",
-    lat: 40.635,
-    lng: -74.0271,
+    lat: 40.6306671,
+    lng: -74.0219105,
   },
 ];
 
