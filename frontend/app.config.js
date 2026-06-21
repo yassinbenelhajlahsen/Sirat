af2be4f5f34a62ef4d1151897ccd2bfc378875ea
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-06-21 02:05:12 -0400

    chore(app): bump version to 1.1.1

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Sirat",
     slug: "Sirat",
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: "portrait",
     icon: "./assets/Icon.jpg",
     splash: {
